829e2d56e90b8dbbc1071b209802141f94533d91
TCM20 <78640690+TCM20@users.noreply.github.com>
2021-02-06 12:17:20 +0000

    Add files via upload
    Mixed Messages Javascript Project

---
+++ mixedMessages.js
@@ -0,0 +1,32 @@
+
+var myArray = [
+    "You will recieve good luck",
+    "A significant person will come into your life",
+    "This week will test your patience"
+  ];
+  
+  var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
+  console.log("I predict that: " + randomItem);
+
+  var luckyItems = [
+      "Blue",
+      "Red",
+      "Orange"
+  ];
+  var randomItem = luckyItems[Math.floor(Math.random()*myArray.length)];
+  console.log("Your Lucky Colour Is: " + randomItem);
+
+var yourNeeds =[
+    "Stay away from any conflict this week",
+    "Speak to the person on your mind to gain clarity",
+    "Allow yourself to express your emotions"
+];
+var randomItem = yourNeeds[Math.floor(Math.random()*myArray.length)];
+  console.log("To progress, you need to: " + randomItem);
+
+    
+
+  
+  
+  
+
